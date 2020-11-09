2ddd33d70e294207f783836d2d018fff71da29ed
herb nadal <harrisonnad64@gmail.com>
2020-11-08 21:16:10 -0500

    Fix runtime error

--- audiobook.py
+++ audiobook.py
@@ -1,15 +1,15 @@
 import pyttsx3
 import PyPDF2
-from tkinter.filedialog import *
+from tkinter.filedialog import askopenfilename
 
-book = askopenfilename()
-pdfreader = PyPDF2.PdfFileReader(book)
-pages = pdfreader.numPages
+if __name__ == "__main__":
+    book = askopenfilename()
+    pdfreader = PyPDF2.PdfFileReader(book)
+    pages = pdfreader.numPages
 
-for num in range(0, pages):
-    page = pdfreader.getPage(num)
-    text = page.extractText()
-    player = pyttsx3.init()
-    player.say(text)
-    player.runAndWait()
-
+    for num in range(0, pages):
+        page = pdfreader.getPage(num)
+        text = page.extractText()
+        player = pyttsx3.init()
+        player.say(text)
+        player.runAndWait()
