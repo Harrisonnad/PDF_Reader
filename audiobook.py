aad10dc1f444f04fef67cd29dc8dcc6a8acc6137
herb nadal <harrisonnad64@gmail.com>
2020-11-08 21:37:47 -0500

    Add Feature input response

--- audiobook.py
+++ audiobook.py
@@ -1,15 +1,26 @@
 import pyttsx3
 import PyPDF2
 from tkinter.filedialog import askopenfilename
+import fire
+
+class Reader(object):
+        
+    def AudioStart(self, page_number):
+
+        book = askopenfilename()
+        pdfreader = PyPDF2.PdfFileReader(book)
+        pages = pdfreader.numPages
+        player = pyttsx3.init()
+        
+        last_page = 0
+        player.say(f"You have decided to start reading from page {page_number}")
+        for num in range(0, pages):
+            page = pdfreader.getPage(num)
+            text = page.extractText()
+            player.say(text)
+            player.runAndWait()
+            last_page = num
+        player.say(f"Your last page read was {last_page}")
 
 if __name__ == "__main__":
-    book = askopenfilename()
-    pdfreader = PyPDF2.PdfFileReader(book)
-    pages = pdfreader.numPages
-
-    for num in range(0, pages):
-        page = pdfreader.getPage(num)
-        text = page.extractText()
-        player = pyttsx3.init()
-        player.say(text)
-        player.runAndWait()
+    fire.Fire()
